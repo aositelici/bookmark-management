34e23cff978f2de24f911083bd8eecb7e075bdf2
liya <liya013@live.com>
2015-11-17 17:44:39 +0800

    js: use regular expression to matching word and replace with highlight

--- src/search.js
+++ src/search.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
+
+  var bookmarks = $("div").html();
+
   $("input:text").bind("input propertychange",function(){
 
     var origin = $(this).val();
-    var length = origin.length;
-    var patten = new RegExp(origin,"ig");
-    var text = $("p").text();
-    while (result = patten.exec(text)){
-      console.log(result);
-    }
+    highLightMatchingword(origin);
   });
 });
+
+function highLightMatchingword(origin) {
+  if(origin !== "") {
+
+    $("p").html(function(i,origText){
+
+      var patten = new RegExp("("+origin+")","ig");
+      return origText.replace(patten,'<span style="background-color:#f54698">'+'$1'+'</span>');
+    });
+  }
+}
