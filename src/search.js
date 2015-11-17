fbe3a1565eeb2bd339c1ea3f0139e7b1c5c859e8
liya <liya013@live.com>
2015-11-17 23:02:27 +0800

    js: add bookmark date

--- src/search.js
+++ src/search.js
@@ -3,11 +3,17 @@ $(document).ready(function(){
   $.getJSON("../bookmark.json",function(result){
 
     $.each(result, function(i, field){
-      var content=$("<p></p>").text(field["title"]);
-      $("div").append(content);
+      var content=$("<p></p>").text(field["title"]).addClass("bookmark");
+      var formatDate = getDate(field["created"]);
+      var date = $("<p></p>").text(formatDate).addClass("date");
+      var line = $("<hr>").addClass("line");
+      $(".content").append(content);
+      $(".content").append(date);
+      $(".content").append(line);
+
     });
 
-    var bookmarks = $("div").html();
+    var bookmarks = $(".content").html();
     $("input:text").bind("input propertychange",function(){
       var origin = $(this).val();
       clearHighLight(bookmarks);
@@ -17,7 +23,7 @@ $(document).ready(function(){
 });
 
 function clearHighLight(bookmarks) {
-  $("div").html(bookmarks);
+  $(".content").html(bookmarks);
 }
 
 function highLightMatchingword(origin) {
@@ -29,4 +35,14 @@ function highLightMatchingword(origin) {
       return origText.replace(patten,'<span style="background-color:#f54698">'+'$1'+'</span>');
     });
   }
+}
+
+function getDate(number) {
+
+  var date = new Date(parseInt(number));
+  var year = date.getFullYear().toString();
+  var month = (date.getMonth()+1).toString();
+  var day = date.getDate().toString();
+  var formatDate = 'created@' + year + '-' + month + '-' + day;
+  return formatDate;
 }
