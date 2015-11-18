78286750e1f2ba0e3cbee42670fbea8ec5029fc3
liya <liya013@live.com>
2015-11-18 12:46:43 +0800

    js: refactor the code

--- src/search.js
+++ src/search.js
@@ -1,69 +1,58 @@
 $(document).ready(function(){
-  $.ajaxSettings.async = false;
-
   var data;
+
+  $.ajaxSettings.async = false;
   $.getJSON("bookmark.json",function(bookmarks){
       data = bookmarks;
   });
 
   $.each(data, function(i, field){
-
-    var content=$("<p></p>").text(field["title"]).addClass("bookmark");
-    var formatDate = getDate(field["created"]);
-    var date = $("<p></p>").text(formatDate).addClass("date");
-    var line = $("<hr>").addClass("line");
-    $(".content").append(content);
-    $(".content").append(date);
-    $(".content").append(line);
+    appendbookmark(field.title,field.created);
   });
 
-var bookmarks = $(".content").html();
-$("input:text").bind("input propertychange",function(){
-  var origin = $(this).val();
-  highLightMatchingword(origin);
+  var bookmarks = $(".content").html();
+  $("input:text").bind("input propertychange",function(){
+    var inputWord = $(this).val();
+    highLightMatchingword(inputWord ,data);
+  });
 });
 
-
-
-
-function clearHighLight(bookmarks) {
-  $(".content").html(bookmarks);
+function appendbookmark(title,createdDate) {
+  var content='<p class="bookmark">' + title + '</p>';
+  var formatDate = getFormatDate(createdDate);
+  var date = $("<p></p>").text(formatDate).addClass("date");
+  var line = $("<hr>").addClass("line");
+  $(".content").append(content);
+  $(".content").append(date);
+  $(".content").append(line);
 }
+
+function getFormatDate(number) {
+  var date = new Date(parseInt(number));
+  return 'created@' + date.getFullYear() + '-' + (date.getMonth()+1) + '-' + (date.getDate()+1);
+}
+
+function highLightMatchingword(inputWord, data) {
+  if(inputWord !== "") {
+    clearHtml();
+    var patten = new RegExp("("+inputWord+")","ig");
+    var filteingData = data.filter(function (subData){
+      return patten.test(subData.title);
+    });
+
+    filteingData.map(function (subData){
+      var highLightBookmark = subData.title.replace(patten,'<span style="background-color:#f54698">'+'$1'+'</span>');
+      appendbookmark(highLightBookmark,subData.created);
+    });
+  } else {
+    showInitBookmark(bookmarks);
+  }
+}
+
 function clearHtml() {
    $(".content").html("");
 }
 
-function highLightMatchingword(origin) {
-  if(origin !== "") {
-    clearHtml();
-    var patten = new RegExp("("+origin+")","ig");
-    var filteingData = data.filter(function (subData){
-      return patten.test(subData.title);
-    });
-    filteingData.map(function (subData){
-
-      var highLightBookmark = subData.title.replace(patten,'<span style="background-color:#f54698">'+'$1'+'</span>');
-      var content='<p class="bookmark">' + highLightBookmark + '</p>';
-      var formatDate = getDate(subData.created);
-      var date = $("<p></p>").text(formatDate).addClass("date");
-      var line = $("<hr>").addClass("line");
-      $(".content").append(content);
-      $(".content").append(date);
-      $(".content").append(line);
-    });
-  }
-  else {
-    clearHighLight(bookmarks);
-  }
+function showInitBookmark(bookmarks) {
+  $(".content").html(bookmarks);
 }
-
-function getDate(number) {
-
-  var date = new Date(parseInt(number));
-  var year = date.getFullYear().toString();
-  var month = (date.getMonth()+1).toString();
-  var day = date.getDate().toString();
-  var formatDate = 'created@' + year + '-' + month + '-' + day;
-  return formatDate;
-}
-});
